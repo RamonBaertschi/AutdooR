9766d013c93ba4c9ae85a6877a5afff1a7f3f826
Ramon <r.baertschi@prc-solutions.ch>
2017-12-03 14:16:16 +0100

    upload with camera plugin

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,26 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+
+        // Kamera
+    document.getElementById("kamera").addEventListener("click", cameraTakePicture);
+    
+        function cameraTakePicture() {
+          navigator.camera.getPicture(onSuccess, onFail, {
+            quality: 100,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: Camera.PictureSourceType.CAMERA
+          });
+    
+          function onSuccess(imageData) {
+            var image = document.getElementById('myImage');
+            image.src = "data:image/jpeg;base64," + imageData;
+          }
+    
+          function onFail(message) {
+            alert('Failed because: ' + message);
+          }
+        }
     },
 
     // Update DOM on a Received Event
