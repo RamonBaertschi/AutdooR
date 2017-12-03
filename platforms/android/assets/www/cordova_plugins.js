97bfa70a87cb5b7169073e0f9247aeb974ba5db4
Ramon <r.baertschi@prc-solutions.ch>
2017-12-03 14:40:17 +0100

    device plugin hinzugefügt

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -31,13 +31,22 @@ module.exports = [
     "clobbers": [
       "CameraPopoverHandle"
     ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-camera": "3.0.0"
+  "cordova-plugin-camera": "3.0.0",
+  "cordova-plugin-device": "1.1.7"
 };
 // BOTTOM OF METADATA
 });
