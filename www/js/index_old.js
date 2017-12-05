b87d102d66364071cbb866448f0932ebfaec4dfb
FabioPitta <abfallundschrott@gmail.com>
2017-12-05 22:59:46 +0100

    index.js

--- www/js/index_old.js
+++ www/js/index_old.js
@@ -48,19 +48,6 @@ var app = {
         alert('Failed because: ' + message);
       }
     }
-
-  },
-
-  // Update DOM on a Received Event
-  receivedEvent: function(id) {
-    var parentElement = document.getElementById(id);
-    var listeningElement = parentElement.querySelector('.listening');
-    var receivedElement = parentElement.querySelector('.received');
-
-    listeningElement.setAttribute('style', 'display:none;');
-    receivedElement.setAttribute('style', 'display:block;');
-
-    console.log('Received Event: ' + id);
   }
 };
 
