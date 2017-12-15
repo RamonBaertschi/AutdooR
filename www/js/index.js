e02b7cc67a6f5c2f39d492db729186bda173785e
Ramon <r.baertschi@prc-solutions.ch>
2017-12-15 20:46:10 +0100

    bug fixing, css, copy clipboard, link zurück

--- www/js/index.js
+++ www/js/index.js
@@ -22,6 +22,8 @@ var app = {
     document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
   },
 
+  
+
   // deviceready Event Handler
   //
   // Bind any cordova events here. Common events are:
