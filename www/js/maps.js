60185bf0819637b4773a715f64865425d4b3d58e
FabioPitta <abfallundschrott@gmail.com>
2017-12-17 16:59:51 +0100

    firebase test

--- www/js/maps.js
+++ www/js/maps.js
@@ -1,22 +1,3 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyARMSjdUYP-5vUVgtb0_rXqtq3kv772ygA",
-  authDomain: "testfabio-3a2e4.firebaseapp.com",
-  databaseURL: "https://testfabio-3a2e4.firebaseio.com",
-  projectId: "testfabio-3a2e4",
-  storageBucket: "testfabio-3a2e4.appspot.com",
-  messagingSenderId: "458824051953"
-};
-firebase.initializeApp(config);
-
-var firestore = firebase.firestore();
-var db;
-
-document.getElementById("startpunkt").addEventListener("click", function() {
-  db = firestore.collection("test1").doc("test2").set("test3");
-  alert("Datenbank");
-})
-
 var data = {
   lat: null,
   lng: null
