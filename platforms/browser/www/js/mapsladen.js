079d9306cb0d55b27508c1748376dd25d3c3609f
Ramon <r.baertschi@prc-solutions.ch>
2017-12-31 13:59:03 +0100

    maps wie admin01 (controls)

--- platforms/browser/www/js/mapsladen.js
+++ platforms/browser/www/js/mapsladen.js
@@ -26,6 +26,10 @@ function initMap(x, y) {
 
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
+    mapTypeId: 'roadmap',
+    streetViewControl: false,
+    fullscreenControl: false,
+    mapTypeControl: false,
     center: new google.maps.LatLng(x, y),
     mapTypeId: 'roadmap'
   });
