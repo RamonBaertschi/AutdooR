1f6ec0f1be9a9b78d3e8037461889e00366e2b09
FabioPitta <abfallundschrott@gmail.com>
2018-01-01 16:19:29 +0100

    .js files auseinander kopiert

--- platforms/browser/www/js/kamera.js
+++ platforms/browser/www/js/kamera.js
@@ -68,25 +68,6 @@ var app = {
 
     console.log('Received Event: ' + id);
   }
-
 };
-/* Toggle Button zum Weitermachen */
-function show() {
-  document.getElementById('div1').style.display = 'inline';
-}
 
-/*Prüfen ob Startpunkt gesetzt*/
-function pruefen() {
-  event.preventDefault();
-  var empt = document.getElementById('koordinaten1').innerHTML;
-  if (empt == null || empt == "")
-  {
-    alert("Bitte einen Startpunkt setzen");
-    return false;
-  }
-  else 
-  {
-    return true; 
-  }
-}
 app.initialize();
---
+++ www/js/mapspruefen.js
@@ -0,0 +1,17 @@
+/* Toggle Button zum Weitermachen */
+function show() {
+  document.getElementById('div1').style.display = 'inline';
+}
+
+/* Prüfen ob Startpunkt gesetzt
+function pruefen(e) {
+  e.preventDefault();
+  var empt = document.getElementById('koordinaten1').innerHTML;
+  if (empt == null || empt == "") {
+    alert("Bitte einen Startpunkt setzen");
+    return false;
+  } else {
+    return true;
+  }
+}
+*/
